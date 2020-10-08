1b601e16ff682f5b1e7b8a9df9bb255a9c161741
burrga01 <hamwannabe17@gmail.com4>
2020-10-08 08:38:28 -0600

    adds uncompleted module code eliminating null reference exception module

---
+++ GameConsole/DimondSkinDefence.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameConsole
+{
+    public class DimondSkinDefence: ISpecialDefence
+    {
+    }
+}
---
+++ GameConsole/ISpecialDefence.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameConsole
+{
+    interface ISpecialDefence
+    {
+        public int CalculateDamageReduction();
+    }
+}
